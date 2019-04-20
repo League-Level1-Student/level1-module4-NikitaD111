10a4f4eaf81b61daa877054f8b7976fb9afbd373
League <league@iMac-6.attlocal.net>
2019-04-19 18:29:42 -0700

    SlotMachine

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -9,10 +9,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SlotMachine implements ActionListener {
 	JButton button = new JButton();
+	JFrame frame = new JFrame();
 	String image1 = "lime.jpeg";
 	String image2 = "download.jpeg";
 	String image3 = "LidoxjjET.jpg";
@@ -21,77 +23,92 @@ public class SlotMachine implements ActionListener {
 	JLabel one = new JLabel();
 	JLabel two = new JLabel();
 	JLabel three = new JLabel();
-public static void main(String[] args) {
-	SlotMachine slot = new SlotMachine();
-	slot.Start();
-	
-}
-void Start() {
-	JFrame frame = new JFrame();
-	frame.setVisible(true);
-	frame.add(panel);
-	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-	/*panel.add(one);
-	panel.add(two);
-	panel.add(three);*/
-	panel.add(button);
-	button.setText("SPIN!");
-	drawButtons();
-	button.addActionListener(this);
-	frame.pack();
-	
-}
-private JLabel createLabelImage(String fileName) throws MalformedURLException{
-    URL imageURL = getClass().getResource(fileName);
-if (imageURL == null){
-	System.err.println("Could not find image " + fileName);
-	return new JLabel();
-}
-Icon icon = new ImageIcon(imageURL);
-JLabel imageLabel = new JLabel(icon);
-return imageLabel;
-}
 
-
-
-
-@Override
-public void actionPerformed(ActionEvent e) {
-	System.out.println("hi");
-	JButton pressed = (JButton) e.getSource();
-	if (button == pressed ) {
+	public static void main(String[] args) {
 		SlotMachine slot = new SlotMachine();
-		slot.drawButtons();
-	}
-	
-}
-	
-void drawButtons() {
-	
+		slot.Start();
 
-	
-	for(int i = 0; i < 3; i++) {
-		int num1 = new Random().nextInt(3);
-		int num2 = new Random().nextInt(3);
-		int num3 = new Random().nextInt(3);
-		if(num1 == 0) {
-		image1 = "download.jpeg";
-		}
-		if(num2 == 1) {
-		image2 = "download.jpeg";
-		}
-		if(num3 == 2) {
-		image3 = "download.jpeg";
-		}
-		
 	}
-	try {
-		panel.add(createLabelImage(image1));
-		panel.add(createLabelImage(image2));
-		panel.add(createLabelImage(image3));
-	} catch (MalformedURLException e) {
-		e.printStackTrace();
+
+	void Start() {
 		
+		frame.setVisible(true);
+		frame.add(panel);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		panel.add(button);
+		button.setText("SPIN!");
+		drawButtons();
+		button.addActionListener(this);
+		frame.pack();
+
 	}
-}
+
+	private JLabel createLabelImage(String fileName) throws MalformedURLException {
+		URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+		Icon icon = new ImageIcon(imageURL);
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("hi");
+		JButton pressed = (JButton) e.getSource();
+		if (button == pressed) {
+			drawButtons();
+		}
+
+	}
+
+	void drawButtons() {
+		panel.remove(one);
+		panel.remove(two);
+		panel.remove(three);
+			int num1 = new Random().nextInt(3);
+		try {
+			if (num1 == 0) {
+				one = createLabelImage(image1);
+			}
+			if (num1 == 1) {
+				one = createLabelImage(image2);
+			}
+			if (num1 == 2) {
+				one = createLabelImage(image3);	
+			}
+			int num2 = new Random().nextInt(3);
+			if (num2 == 0) {
+				two = createLabelImage(image1);
+			}
+			if (num2 == 1) {
+				two = createLabelImage(image2);
+			}
+			if (num2 == 2) {
+				two = createLabelImage(image3);	
+			}
+			int num3 = new Random().nextInt(3);
+			if (num3 == 0) {
+				three = createLabelImage(image1);
+			}
+			if (num3 == 1) {
+				three = createLabelImage(image2);
+			}
+			if (num3 == 2) {
+				three = createLabelImage(image3);	
+			}
+			panel.add(one);
+			panel.add(two);
+			panel.add(three);
+			frame.pack();
+			if (num1 == num2 && num2 == num3) {
+				JOptionPane.showMessageDialog(null, "You won bye");
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+
+		}
+	}
 }
